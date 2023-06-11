cb58bd942e06242f8f886d8fb7473360d41a7548
Marcos Felipe <mf7778838@gmail.com>
2023-06-10 21:40:36 -0300

    Queen

--- src/chess/pieces/Queen.java
+++ src/chess/pieces/Queen.java
@@ -5,9 +5,9 @@ import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-public class Rook extends ChessPiece {
+public class Queen extends ChessPiece {
 
-	public Rook(Board board, Color color) {
+	public Queen(Board board, Color color) {
 		super(board, color);
 	}
 
@@ -53,11 +53,49 @@ public class Rook extends ChessPiece {
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			mat[p.getRow()][p.getCollumn()] = true;
 		}
+
+		// nw
+		p.setValues(position.getRow() - 1, position.getCollumn() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getCollumn()] = true;
+			p.setValues(p.getRow() - 1, p.getCollumn() - 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getCollumn()] = true;
+		}
+		// se
+		p.setValues(position.getRow() - 1, position.getCollumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getCollumn()] = true;
+			p.setValues(p.getRow() - 1, p.getCollumn() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getCollumn()] = true;
+		}
+		// se
+		p.setValues(position.getRow() + 1, position.getCollumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getCollumn()] = true;
+			p.setValues(p.getRow() + 1, p.getCollumn() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getCollumn()] = true;
+		}
+		// sw
+		p.setValues(position.getRow() + 1, position.getCollumn() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getCollumn()] = true;
+			p.setValues(p.getRow() + 1, p.getCollumn() - 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getCollumn()] = true;
+		}
+
 		return mat;
 	}
 
 	@Override
 	public String toString() {
-		return "R";
+		return "Q";
 	}
 }
